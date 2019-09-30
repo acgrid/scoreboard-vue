2c7b4ed8bb92cff60757b3bca11a9b05c9cf0e65
acgrid <acgrid@gmail.com>
2019-10-01 01:59:26 +0800

    Chore: update server URL

--- src/api/base.js
+++ src/api/base.js
@@ -1,1 +1,1 @@
-export default process.env.NODE_ENV === 'production' ? 'https://scoreboard.comitime.com/api' : 'http://192.168.0.110:3003'
+export default process.env.NODE_ENV === 'production' ? 'https://sc.comitime.com/' : 'http://192.168.0.110:3003'
