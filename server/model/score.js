f8ca47360f26d034e51fae71b3ff03ae85de4d1d
acgrid <acgrid@gmail.com>
2019-09-30 22:35:34 +0800

    Fix: Backend problems

--- server/model/score.js
+++ server/model/score.js
@@ -1,18 +1,33 @@
 import mongoose, { Schema } from 'mongoose'
 import { Score, Contest as ContestRef, Judge as JudgeRef, Candidate as CandidateRef, Evaluation as EvaluationRef } from './index'
-import { pack } from '../../lib/score'
+import Contest from './contest'
+import fk from './traits/foreign-key'
 
 export const schema = new Schema({
-  contest: { type: Schema.Types.ObjectId, ref: ContestRef, required: true },
-  judge: { type: Schema.Types.ObjectId, ref: JudgeRef, required: true },
-  candidate: { type: Schema.Types.ObjectId, ref: CandidateRef, required: true },
-  evaluation: { type: Schema.Types.ObjectId, ref: EvaluationRef, required: true },
+  contest: { type: String, ref: ContestRef, required: true },
+  judge: { type: String, ref: JudgeRef, required: true },
+  candidate: { type: String, ref: CandidateRef, required: true },
+  evaluation: { type: String, ref: EvaluationRef, required: true },
   score: { type: Number, required: true },
   modified: { type: [Number], default: [] }
-}, { timestamps: true, toObject: { virtuals: true } })
-
-schema.virtual('path').get(pack)
+}, { timestamps: true })
 
 schema.index({ contest: 1, judge: 1, candidate: 1, evaluation: 1 }, { unique: true })
+
+schema.pre('save', async function () {
+  await fk(Contest, this.contest)
+  const contest = await Contest.findById(this.contest)
+  if (!contest) throw new Error('赛事不存在')
+  console.log(contest.groups)
+  if (contest.judges.indexOf(this.judge) === -1) throw new Error('非赛事评委')
+  if (contest.groups.reduce((candidates, group) => {
+    candidates.push(...group.candidates)
+    return candidates
+  }, []).indexOf(this.candidate) === -1) throw new Error('非参赛选手')
+  if (contest.evaluations.reduce((evaluations, group) => {
+    evaluations.push(...group.items)
+    return evaluations
+  }, []).indexOf(this.evaluation) === -1) throw new Error('非评分项目')
+})
 
 export default mongoose.model(Score, schema)
--- server/socket.js
+++ server/socket.js
@@ -11,38 +11,46 @@ const dbg = debug('sc:socket')
 export default function (http) {
   const server = io(http)
   server.on('connection', function (socket) {
+    dbg('connection', socket.id)
     const session = { contest: null, judge: null }
-    socket.on('auth', async (transport, ack) => {
-      const { judge, password, contest } = transport
-      const contestData = await Contest.findById(contest).populate('judges')
-      if (!contestData) return ack({ error: '赛事不存在' })
-      if (!contestData.judges.find(j => j._id === judge)) return ack({ error: '不是本赛事评委' })
-      if (!await Judge.exists({ _id: judge, password })) return ack({ error: '评委密码错误' })
-      session.judge = judge
-      session.contest = contest
+    socket.on('auth', async (contest, judge, password, ack) => {
+      dbg(`Auth: ${contest}: ${judge} with password ${password}`)
+      const resp = {}
+      resp.contest = await Contest.findById(contest).populate('judges', '-password').populate('evaluations.items').populate('groups.candidates')
+      if (!resp.contest) return ack({ error: '赛事不存在' })
+      if (!resp.contest.judges.find(j => j._id === judge)) return ack({ error: '不是本赛事评委' })
+      resp.judge = await Judge.findOne({ _id: judge, password }, '-password')
+      if (!resp.judge) return ack({ error: '评委密码错误' })
       socket.join(contest, async error => {
         if (error) return ack({ error })
-        contestData.scores = await Score.find({ contest }, 'judge evaluate candidate score modified')
-        ack(contestData)
+        session.judge = judge
+        session.contest = contest
+        resp.scores = await Score.find({ contest }, 'judge evaluation candidate score modified')
+        ack(resp)
       })
     })
-    socket.on('score', async (transport, ack) => {
+    socket.on('score', async (evaluation, candidate, score, ack) => {
       if (session.contest && isJudge(session.judge)) {
+        dbg(`Score: ${session.contest}: ${session.judge} : ${candidate} : ${evaluation} : ${score}`)
+        if (!evaluation || !candidate || Number.isNaN(score)) return
         let s
-        const { evaluate, candidate, score } = transport
-        if (!evaluate || !candidate) return ack({ error: 'Missing' })
-        const primary = { ...session, evaluate, candidate }
+        const primary = { ...session, evaluation, candidate }
         const pervious = await Score.findOne(primary)
-        if (pervious) {
-          s = pervious
-          s.modified.push(s.score)
-          s.score = score
-          await s.save()
-        } else {
-          s = await Score.create({ ...primary, score })
+        try {
+          if (pervious) {
+            s = pervious
+            s.modified.push(s.score)
+            s.score = score
+            await s.save()
+          } else {
+            s = await Score.create({ ...primary, score })
+          }
+          socket.to(session.contest).emit('score', s)
+          ack({ s })
+        } catch (e) {
+          dbg('Error', e)
+          ack({ e })
         }
-        socket.to(session.contest).emit('score', s)
-        ack(s)
       }
     })
   })
