90b99980287ad9202607a80cad161c5de2e21f9d
acgrid <acgrid@gmail.com>
2019-10-18 11:13:17 +0800

    Chore: change environment

--- config/default.js
+++ config/default.js
@@ -3,7 +3,7 @@ module.exports = {
   production,
   secure: true, // in case of API service is https proxy passed
   mongo: {
-    dsn: `mongodb://${production ? 'mongodb:27017' : 'root:test@localhost:27017'}/scoreboard`,
+    dsn: `mongodb://${production ? 'mongodb:27017' : 'root:test@localhost:27017'}/scoreboard-final`,
     options: { authSource: 'admin', auth: { user: 'root', password: 'test' } }
   }
 }
--- src/api/base.js
+++ src/api/base.js
@@ -1,1 +1,1 @@
-export default process.env.NODE_ENV === 'production' ? 'https://sc.comitime.com/' : 'http://192.168.0.110:3003'
+export default process.env.NODE_ENV === 'production' ? 'https://sc.comitime.com/' : 'http://127.0.0.1:3003'
